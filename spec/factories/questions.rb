45dee7ee63acf1712ef81250dceb4fca9aa4dd32
_nil <0nil@inbox.ru>
2017-08-08 22:28:19 +0300

    add & config rubocop

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryGirl.define do
   factory :question do
-    title "MyString"
-    body "MyText"
+    title 'MyString'
+    body 'MyText'
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
-require 'shoulda-matchers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -30,10 +31,6 @@ ActiveRecord::Migration.maintain_test_schema!
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
-
-    # with.library :active_record
-    # with.library :active_model
-
     with.library :rails
   end
 end
