b2d87676f194885e4309ea6afb8c07d4e89b166c
_nil <0nil@inbox.ru>
2017-08-12 11:49:12 +0300

    show question spec

--- spec/acceptance/view_question_list_spec.rb
+++ spec/acceptance/view_question_list_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-feature 'Anyone can view the list of questions', %q{
-  Anyone can view the list of questions
-  to find the question that interests him.
+feature 'Show question', %q{
+  Any user can view the question of interest
+  to give an answer or see the answers of other users.
 } do
 
-  scenario 'View question list' do
-    visit questions_path
+  scenario 'show question' do
+    visit question_path
 
-    expect(current_path).to eq questions_path
+    expect(current_path).to eq question_path
     expect(page).to have_content 'Список вопросов'
   end
 end
