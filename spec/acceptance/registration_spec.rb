f2065f333135c1afc17945d74fd1d183d69b33f5
_nil <0nil@inbox.ru>
2017-08-13 09:43:49 +0300

    registaration spec

---
+++ spec/acceptance/registration_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'Guest registration', %q{
+  A guest can register on the system to ask questions and answers
+} do
+  scenario 'Guest trying to register' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(current_path).to eq root_path
+  end
+end
