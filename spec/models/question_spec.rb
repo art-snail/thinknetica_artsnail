d675e2568c63ebdf71419b467c943de988117f6b
_nil <0nil@inbox.ru>
2017-08-25 17:34:10 +0300

    Attachment model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
+  it { should have_many :attachments }
 
   it { should belong_to :user }
 
