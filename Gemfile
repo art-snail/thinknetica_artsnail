64e2b0f5388466875bd73fcd98014a59e232e7ad
_nil <0nil@inbox.ru>
2017-09-12 17:03:53 +0300

    oauth facebook

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'gon'
 gem 'skim'
 gem 'materialize-sass'
 gem 'responders'
+gem 'omniauth'
+gem 'omniauth-facebook'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -53,7 +55,8 @@ group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'web-console', '>= 3.3.0'
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  # Spring speeds up development by keeping your application running in the background.
+  # Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should have_many(:questions).dependent(:destroy) }
-  it { should have_many(:answers) }
+  it { should have_many :answers }
+  it { should have_many :authorizations }
 
   let(:user) { create(:user)}
   let(:question) {create(:question, user: user)}
@@ -14,5 +15,74 @@ RSpec.describe User, type: :model do
 
   it 'check author_of? for not author' do
     expect(other_user).to_not be_author_of(question)
+  end
+
+  describe 'find_for_oauth' do
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
+
+    context 'user alredy has autorization' do
+      it 'returns the user' do
+        user.authorizations.create(provider: 'facebook', uid: '123456')
+        expect(User.find_for_oauth(auth)).to eq user
+      end
+    end
+
+    context 'user has not authorization' do
+      context 'user already exists' do
+        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook',
+                                            uid: '123456',
+                                            info: { email: user.email }) }
+        it 'does not create new user' do
+          expect { User.find_for_oauth(auth) }.to_not change(User, :count)
+        end
+
+        it 'creates authorization for user' do
+          expect { User.find_for_oauth(auth) }.to change(user.authorizations, :count).by(1)
+        end
+
+        it 'creates authorization with provider and uid' do
+          authorization = User.find_for_oauth(auth).authorizations.first
+
+          expect(authorization.provider).to eq auth.provider
+          expect(authorization.uid).to eq auth.uid
+        end
+
+        it 'returns the user' do
+          expect(User.find_for_oauth(auth)).to eq user
+        end
+      end
+
+      context 'user does not exists' do
+        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook',
+                                            uid: '123456',
+                                            info: { email: 'new@user.com' }) }
+
+        it 'create new user' do
+          expect { User.find_for_oauth(auth) }.to change(User, :count).by(1)
+        end
+
+        it 'returns new user' do
+          expect(User.find_for_oauth(auth)).to be_a(User)
+        end
+
+        it 'fills user email' do
+          user = User.find_for_oauth(auth)
+          expect(user.email).to eq auth.info.email
+        end
+
+        it 'creates authorization for user' do
+          user = User.find_for_oauth(auth)
+          expect(user.authorizations).to_not be_empty
+        end
+
+        it 'creates authorization with provider and uid' do
+          authorization = User.find_for_oauth(auth).authorizations.first
+
+          expect(authorization.provider).to eq auth.provider
+          expect(authorization.uid).to eq auth.uid
+        end
+      end
+    end
   end
 end
