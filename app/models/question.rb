1a38154d881d0b2d5dfbd78c665cf7f1a474dda1
_nil <0nil@inbox.ru>
2017-10-05 20:20:13 +0300

    test

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@
 class Question < ApplicationRecord
   include Votable
 
-  has_many :answers, dependent: :destroy
+  # has_many :answers, dependent: :destroy
   has_many :attachments, as: :attachable, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
 
